dd48fc31264eab0ea44d543c8da16580360f1108
양동규 <itsydev12@gmail.com>
2026-01-05 16:32:47 +0900

    feat: 프록시, svgr 설정

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  // 프록시 설정
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: "http://metafairy.me:3600/api/v1/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
-
