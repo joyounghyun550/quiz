56d5dffcb4e6bd16b095e586f3d698e9f12b72b9
양동규 <itsydev12@gmail.com>
2026-01-05 17:19:38 +0900

    feat: commonType, axios 설정

---
+++ src/shared/types/common.type.ts
@@ -0,0 +1,15 @@
+/**
+ * 공통 타입 정의
+ */
+
+export type ApiResponse<T> = {
+    data: T;
+    message?: string;
+    statusCode: number;
+  };
+  
+  export type ApiError = {
+    message: string;
+    statusCode: number;
+    errors?: Record<string, string[]>;
+  };
